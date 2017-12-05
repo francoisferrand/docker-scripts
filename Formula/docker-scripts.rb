0931b7d36337b83ceaf3db4aff727888e4b56871
Francois Ferrand <thetypz@gmail.com>
2017-12-05 11:21:25 +0100

    Version 1.2

--- Formula/docker-scripts.rb
+++ Formula/docker-scripts.rb
@@ -2,7 +2,7 @@ class DockerScripts < Formula
   desc "A collection of scripts which simplify working with docker."
   homepage "https://github.com/Typz/docker-scripts"
 
-  url "https://github.com//Typz/docker-scripts.git", :tag => "1.1"
+  url "https://github.com//Typz/docker-scripts.git", :tag => "1.2"
   bottle :unneeded
 
   head "https://github.com/Typz/docker-scripts.git"
