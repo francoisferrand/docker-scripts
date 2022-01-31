e50f61fb7d0181e6356e66f8eb020b5c545e3c00
Francois Ferrand <thetypz@gmail.com>
2022-01-31 09:07:01 +0100

    Remove deprecated homebrew command

--- Formula/docker-scripts.rb
+++ Formula/docker-scripts.rb
@@ -3,17 +3,15 @@ class DockerScripts < Formula
   homepage "https://github.com/Typz/docker-scripts"
 
   url "https://github.com//Typz/docker-scripts.git", :tag => "1.3"
-  bottle :unneeded
-
   head "https://github.com/Typz/docker-scripts.git"
 
   def install
-	bin.install "docker-certs"
-	bin.install "docker-env"
-	bin.install "docker-copy-images"
-  bin.install "kubectx-delete"
-  bin.install "kubectx-import"
+    bin.install "docker-certs"
+    bin.install "docker-env"
+    bin.install "docker-copy-images"
+    bin.install "kubectx-delete"
+    bin.install "kubectx-import"
 
-	bash_completion.install "completion/docker-env.bash" => "docker-env"
+    bash_completion.install "completion/docker-env.bash" => "docker-env"
   end
 end
